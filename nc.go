1f174803acae5096ab68f93ded3f1b99066f822e
Scott Lawrence <bytbox@gmail.com>
2011-12-27 02:31:19 -0500

    Adding -V flag

--- nc.go
+++ nc.go
@@ -3,8 +3,21 @@ package main
 import (
 	//"crypto/tls"
 	"flag"
+	"fmt"
+	//"net"
+)
+
+const VERSION = `0.1`
+
+// Flags
+var (
+	version = flag.Bool("V", false, "Display version information and exit")
 )
 
 func main() {
 	flag.Parse()
+
+	if *version {
+		fmt.Printf("netcat-tls %s\n", VERSION)
+	}
 }
