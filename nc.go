9965a42f0447e8a1791cc6a9451126aef6a34d06
Scott Lawrence <bytbox@gmail.com>
2011-12-27 02:43:04 -0500

    Non-TLS communication sorta works

--- nc.go
+++ nc.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	//"crypto/tls"
 	"flag"
 	"fmt"
-	//"net"
+	"io"
+	"net"
+	"os"
 )
 
 const VERSION = `0.1`
@@ -20,4 +23,42 @@ func main() {
 	if *version {
 		fmt.Printf("netcat-tls %s\n", VERSION)
 	}
+
+	conn, err := net.Dial("tcp", "google.com:80")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	go func() {
+		r := bufio.NewReader(os.Stdin)
+		for {
+			l, _, err := r.ReadLine()
+			if err != nil {
+				os.Exit(0)
+			}
+			_, err = conn.Write([]byte(string(l)+"\n"))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+	}()
+
+	go func() {
+		r := bufio.NewReader(conn)
+		for {
+			l, _, err := r.ReadLine()
+			if err != nil {
+				if err == io.EOF {
+					os.Exit(0)
+				}
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			fmt.Print(string(l))
+		}
+	}()
+
+	<-make(chan interface{})
 }
